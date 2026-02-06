e200a6518af109f37cbfb8fe464ec1bcae7293e1
natively.dev <api@natively.dev>
2026-02-06 12:38:06 +0000

    feat: Add HapticTab, TabBarBackground, useColorScheme and fix errorLogger

---
+++ components/HapticTab.tsx
@@ -0,0 +1,21 @@
+
+import { ComponentProps } from 'react';
+import { Platform } from 'react-native';
+import * as Haptics from 'expo-haptics';
+import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
+import { PlatformPressable } from '@react-navigation/elements';
+
+export function HapticTab(props: BottomTabBarButtonProps) {
+  return (
+    <PlatformPressable
+      {...props}
+      onPressIn={(ev) => {
+        if (Platform.OS === 'ios') {
+          // Add a soft haptic feedback when pressing a tab on iOS
+          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+        }
+        props.onPressIn?.(ev);
+      }}
+    />
+  );
+}
